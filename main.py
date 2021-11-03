60b6182b25264f45101dc3d4d6f138b931ee716a
Elh-Ayoub <ayoub1998elh@gmail.com>
2021-11-03 16:45:48 +0200

    Add task 4

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import csv
 import math
+import time
 
 def read_csv():
     field = []
@@ -36,7 +37,51 @@ def exec_rand(field, g):
             if(field[i][j] == 0 or field[i][j] == 3):
                 x = random.choices([0, 3], g)[0]
                 field[i][j] = x
+    field = herbivores_behavior(field)
+    time.sleep(0.5)
     return field
+
+def herbivores_behavior(field):
+    for i in range(len(field)):
+        for j in range(len(field[i])):
+            if(field[i][j] == 1):
+                #Top cell
+                if(i - 2 < 0): t_cell = 0
+                else: t_cell = i-2
+                #Right cell
+                if(j + 3 >= len(field[i])): r_cell = len(field[i])
+                else: r_cell = j + 3
+                #Bottom cell
+                if(i + 3 >= len(field)): b_cell = len(field)
+                else: b_cell = i + 3
+                #Left cell
+                if(j - 2 < 0): l_cell = 0
+                else: l_cell = j - 3
+
+                plants = 0
+                herbivorous = 0
+                for k in range(t_cell, b_cell):
+                    for l in range(l_cell, r_cell):
+                        if (i, j) != (k, l):
+                            if(field[k][l] == 3): plants += 1
+                            elif(field[k][l] == 1): herbivorous += 1
+                if herbivorous >= 1 and plants >= 4:
+                    plants =4
+                    herbivorous = 1
+                    x = random.randrange(t_cell, b_cell)
+                    y =  random.randrange(l_cell, r_cell)
+                    while plants or herbivorous:
+                        if((field[x][y] == 3) and plants):
+                            field[x][y] = 0
+                            plants -= 1
+                        elif((field[x][y] == 0) and herbivorous):
+                            field[x][y] = 1
+                            herbivorous -= 1
+                        x = random.randrange(t_cell, b_cell)
+                        y = random.randrange(l_cell, r_cell)
+            else:
+                continue
+    return field 
 
 def print_iter(field, n):
     print("Borad number {0}: ".format(n+1))
